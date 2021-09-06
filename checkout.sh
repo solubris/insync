9eb128b22622c80c976c2041cea5e4cc100d4e10
Tim Walters <tim@solubris.com>
2021-09-06 21:41:13 +0100

    pass in the token 2

--- checkout.sh
+++ checkout.sh
@@ -5,8 +5,8 @@ TEMP_PATH="$(mktemp -d /tmp/insync.XXXXXX)"
 
 cd "$TEMP_PATH"
 
-git config user.name github-actions
-git config user.email github-actions@github.com
+#git config user.name github-actions
+#git config user.email github-actions@github.com
 git clone https://$3:x-oauth-basic@github.com/$1/$2.git
 
 cd "$2"
