e017bcf2196b501d2a3c416ff73c0f56242c7dd8
Tim Walters <tim@solubris.com>
2021-09-06 22:07:27 +0100

    dont create sub dir

--- checkout.sh
+++ checkout.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-SCRIPT_PATH=$(dirname "$0")
-
 #git config user.name github-actions
 #git config user.email github-actions@github.com
 # TODO use this var: GITHUB_SERVER_URL
 
-git clone "https://$2:x-oauth-basic@github.com/$1.git"
-
-#cd "$2"
-
+git clone "https://$2:x-oauth-basic@github.com/$1.git" .
