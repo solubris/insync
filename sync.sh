1630deaced3c76d9ed74f5eb2cd0d8aee65aae16
Tim Walters <tim@solubris.com>
2021-09-15 22:54:45 +0100

    try to fix matching branch check 2

--- sync.sh
+++ sync.sh
@@ -44,6 +44,8 @@ for f in ${files[*]}; do
 done
 
 # don't push changes if there is already a branch for this change
+$SCRIPT_PATH/has-branch.sh "$DST_PATH"
+
 matchingBranches=$($SCRIPT_PATH/has-branch.sh "$DST_PATH")
 if [ $matchingBranches -ne 0 ]; then
   echo 'branch already exists, will not do anything'
