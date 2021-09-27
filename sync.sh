8029cd8489df89e66c09d1a14ac28fcf042936ce
Tim Walters <tim@solubris.com>
2021-09-27 12:59:19 +0100

    testUpdateDir 2

--- sync.sh
+++ sync.sh
@@ -9,8 +9,8 @@ files=($*)
 for f in ${files[*]}; do
   if [ -e "$SRC_PATH/$f" ]; then
     echo "copying file to dst: $f"
-    cp "$SRC_PATH/$f" "$DST_PATH/"
-    cd "$DST_PATH"; git add "$f"
+    cp -r "$SRC_PATH/$f" "$DST_PATH/"
+#    cd "$DST_PATH"; git add "$f"
   else
     echo "file doesn't exist in src, removing from dest: $f"
     rm -f "$DST_PATH/$f"
